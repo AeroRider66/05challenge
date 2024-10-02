f81f62ab29b788b745dd0f7a44cd7669bbbcd6ff
AeroRider <dave.butterworth.ee@gmail.com>
2024-10-01 22:25:47 -0500

    trying to figure how to create card object, and figure out egg/chicken on the how to get the unix converted time into the object. Also how to inialize. Maybe innerHTML?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -9,27 +10,57 @@ const taskDueDate = document.querySelector("#taskDueDate");
 const taskDescription = document.querySelector("#taskDescription");
 const taskStatus = document.querySelector("#taskStatus");
 
+let taskObject = {
+    taskId: UniqueIdTask,
+    taskTitle: titleTask,
+    taskDueDate: taskDueDate,
+    taskDescription: taskDescription,
+    taskStatus: taskStatus
+}
+
 // Todo: create a function to generate a unique task id
 // Use Date.now to use for unique task id
 function generateTaskId() {
     // grabs unix time to use as unique ID
-    uniqueTaskId = dayjs();
-    console.log("using dayjs uniqueId = "+uniqueTaskId);
+    const taskUniqueId = dayjs();
+    console.log("using dayjs uniqueId = "+taskUniqueId);
     // converts unix time to local timestamp (only down to seconds)
-    const unixDateTime = Date(uniqueTaskId.toString());
+    const unixDateTime = Date(taskUniqueId.toString());
     console.log("using Date = "+unixDateTime);
     // could also use (this is without using dayjs):  const dayUniqueId = Date(Date.now()).toString();
 
-    return uniqueTaskId,
+    return [taskUniqueId, unixDateTime];
 }
+
+// when 'add task' button is pushed, need to execute this.
 generateTaskId()
+console.log(taskUniqueId);
+const [taskUniqueId, unixDateTime] = generateTaskId
+
 
 // Todo: create a function to create a task card
 // use div id="todo-cards" or "in-progress-cards" or "done-cards" to define which column
 //
 function createTaskCard(task) {
+
+    const putCardTogether =
     let taskId = document.createElement("div");
     let taskTitle = document.createElement("div");
+
+    function createTaskElement(taskId, taskTitle, taskDueDate, taskDescription, taskStatus) {
+        const taskId = id
+        const task = document.createElement("div");
+        task.id = taskId;
+        task.classTaskName = "task";
+        task.draggableTast = true;
+        task.innerHTML =
+            `${content}
+    <span class="delete-btn" 
+        onclick="deleteTask('${taskId}')">
+    </span>`;
+        task.addEventListener("dragstart", drag);
+        return task;
+    }
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -56,3 +87,37 @@ function handleDrop(event, ui) {
 $(document).ready(function () {
 
 });
+
+
+
+// // Retrieve tasks and nextId from localStorage
+// let taskList = JSON.parse(localStorage.getItem("tasks"));
+// let nextId = JSON.parse(localStorage.getItem("nextId"));
+//
+// // Assign references to important DOM elements
+// const taskId = document.querySelector("#task-id");
+// const taskTitle = document.querySelector("#taskTitle");
+// const taskDueDate = document.querySelector("#taskDueDate");
+// const taskDescription = document.querySelector("#taskDescription");
+// const taskStatus = document.querySelector("#taskStatus");
+//
+//
+//
+// // Todo: create a function to generate a unique task id
+// // Use Date.now to use for unique task id
+// function generateTaskId() {
+//     // grabs unix time to use as unique ID
+//     uniqueTaskId = dayjs();
+//     console.log("using dayjs uniqueId = "+uniqueTaskId);
+//     // converts unix time to local timestamp (only down to seconds)
+//     const unixDateTime = Date(uniqueTaskId.toString());
+//     console.log("using Date = "+unixDateTime);
+//     // could also use (this is without using dayjs):  const dayUniqueId = Date(Date.now()).toString();
+//
+//     return uniqueTaskId,
+// }
+//
+// // when 'add task' button is pushed, need to execute this.
+// generateTaskId();
+// console.log(uniqueTaskId);
+//
